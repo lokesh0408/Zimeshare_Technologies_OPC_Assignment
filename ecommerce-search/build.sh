8025d1362f1218bc5173ee72a09ec2d61e5bb8c8
lokesh0408 <lokeshjindal997@gmail.com>
2024-06-09 18:00:44 +0530

    Grant execute permissions to build.sh

--- ecommerce-search/build.sh
+++ ecommerce-search/build.sh
@@ -7,8 +7,8 @@ composer install --no-dev --optimize-autoloader
 npm install
 npm run prod
 
-# Export the site to static files
-php artisan export
+# Run database migrations
+php artisan migrate --force
 
-# Move exported files to the public directory for Netlify to serve
-mv public/exported public
+# Run database seeds
+php artisan db:seed --force
